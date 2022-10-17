ec3beca93f28f16110e333a79bda1666715105f5
unknown <joshuamorrison623@gmail.com>
2022-10-17 15:53:05 -0400

    Jeopardy-app

---
+++ react/Jeopardy-app/src/Jeopardy.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+export default function Jeopardy(props) {
+
+    
+
+
+
+
+
+    return (
+        <>
+            <h1>Welcome to Jeopardy</h1>
+            <h2>Score: {Number}</h2>
+            <section>
+            <br />
+            <button onClick={'text'}>Decrease</button>
+            <button onClick={'text'}>Increase</button>
+            <button onClick={'text'}>Reset</button>
+            </section>
+        </>
+    )
+}
